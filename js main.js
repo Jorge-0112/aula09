ef802f9fe8c974dfdfd4983d7c2aca2acc05f3dd
Jorge-0112 <164198191+Jorge-0112@users.noreply.github.com>
2024-04-10 11:19:13 -0300

    Add files via upload

---
+++ js main.js
@@ -0,0 +1,10 @@
+//função somar 
+function somar(){
+var num1=documment.getElemntebyId("n1").value;
+var num2=documment.getElemntebyId("n1").value;
+var resultado=document.createElement('o Resultadoda soma entre ${num1} + : ${num2} é iqual a: ${resu}')
+}
+
+
+
+
---
+++ sketch.js
@@ -0,0 +1,20 @@
+//window.alert("meu primeiro alerta")//criaçaõ de alerta
+//window.confirm("está gostando Curso?")//criação de confirmação 
+//window.prompt("qual seu nome?")//criação de caixa de pergunta
+//windo.alert(' seja bem vindo'+ nome'+!')
+
+//criando 2 números 
+//var n1 =windows.parseInt(windo.prompt('digite um numero)
+//var n2 =window.parseint(window.prompt(Digite outro número')
+// var s=n1n2
+//var s=window.alert9('A soma dos 2 Números é ' + s)
+
+//javascript decide se é inteiro ou flutuante
+//var n1=number(windows.prompt('digite um numero')
+//var n2=number(windows.prompt(digite outro número')
+//var=n1+n2
+//var=swindows.alert('A soma dos 2 Números é ' +s)
+//javascript decide se é inteiro ou flutuante
+// var n1= numember(windows.prompt)
+
+
